452dffdfd5cd9807d0147387afac4fdb5f533c8a
cohouseol <cohouseol@gmail.com>
2022-09-05 19:36:36 +0900

    1

---
+++ was-practice/src/main/java/org/example/HttpRequest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.example.calculate.RequestLine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class HttpRequest {
+    private final RequestLine requestLine;
+
+    public HttpRequest(BufferedReader br) throws IOException {
+        this.requestLine = new RequestLine(br.readLine());
+    }
+
+    public QueryStrings getQueryString() {
+        return requestLine.getQueryStrings();
+    }
+
+    public boolean isGetRequest() {
+        return requestLine.isGetRequest();
+    }
+
+    public boolean matchPath(String requestPath) {
+        return requestLine.matchPath(requestPath);
+
+    }
+}
