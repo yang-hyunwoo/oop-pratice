deeb10bc48d29fc79eec76c77e9be5748f4fb538
cohouseol <cohouseol@gmail.com>
2022-09-05 15:08:37 +0900

    s
    s

---
+++ src/main/java/org/example/customer/Cooking.java
@@ -0,0 +1,10 @@
+package org.example.customer;
+
+public class Cooking {
+    public Cook makeCook(MenuItem menuItem) {
+        Cook cook = new Cook(menuItem);
+        return cook;
+    }
+
+
+}
---
+++ src/main/java/org/example/customer/Customer.java
@@ -0,0 +1,11 @@
+package org.example.customer;
+
+public class Customer {
+
+    public void order(String menuName,Menu menu , Cooking cooking){
+        MenuItem menuItem = menu.choose(menuName);
+        Cook cook = cooking.makeCook(menuItem);
+
+
+    }
+}
